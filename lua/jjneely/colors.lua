bb8d0ab22464083b43873e50913dd0685c3f0e33
Jack Neely <jjneely@42lines.net>
2022-03-17 23:36:35 -0400

    Activate darkplus color scheme
    and don't go insane.  Background is black, damnit!

--- lua/jjneely/colors.lua
+++ lua/jjneely/colors.lua
@@ -1,3 +1,13 @@
 vim.cmd [[
     match errorMsg /\s\+$/
+    set background=dark
+    try
+      colorscheme darkplus
+      hi Normal ctermbg=NONE guibg=NONE
+      hi LineNr ctermfg=7 guifg=normal
+      hi LineNrAbove ctermfg=11 guifg=#808080
+      hi LineNrBelow ctermfg=11 guifg=#808080
+    catch /^Vim\%((\a\+)\)\=:E185/
+      colorscheme default
+    endtry
 ]]
